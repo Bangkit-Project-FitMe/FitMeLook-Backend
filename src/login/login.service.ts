d6dc12416f8701a7342488d2ef29cc9bb9360075
IrsyadFikriansyah <irsyadfikriansyah@gmail.com>
2024-06-05 20:30:28 +0700

    [feat] add login api

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,12 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
 @Injectable()
 export class LoginService {
-  userLogin(email: string, password: string) {
+  private firestore: admin.firestore.Firestore;
+
+  constructor(@Inject('FIREBASE_ADMIN') private firebaseAdmin: admin.app.App) {
+    this.firestore = firebaseAdmin.firestore();
+  }
+
+  async userLogin(email: string, password: string) {
+    const snapshot = await this.firestore.collection('users').get();
+
+    const fetchedData = snapshot.docs.map((doc) => doc.data());
+    const user = fetchedData.filter((doc) => doc.email === email)[0];
+    if (user) {
+      if (user.password === password) {
+        return {
+          status: 'success',
+          message: 'login successfully.',
+        };
+      }
+    }
+
     return {
-      message: 'login successfully',
-      email: email,
-      password: password,
+      status: 'failed',
+      message: 'email or password is incorrect',
     };
   }
 }
