874009eda825a23bbb6cc85762a8b5609d9f2475
IrsyadFikriansyah <irsyadfikriansyah@gmail.com>
2024-06-05 20:30:08 +0700

    [feat] add user/:id/predict api

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ModelModule } from 'src/module/model.module';
+import { FirestoreModule } from 'src/firestore/firestore.module';
 
 @Module({
+  imports: [ModelModule, FirestoreModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
