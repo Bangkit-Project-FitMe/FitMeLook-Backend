b140390b227129af2d4c84a9456cab30ff8729d4
Arya Wibowo Jati <aryajati29@gmail.com>
2024-06-09 13:08:54 +0700

    refactor: change predictions structure, fix typo file, and date

--- src/prediction/prediction.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { PredictionController } from "./prediction.controller";
-import { PredictionService } from "./prediction.service";
-@Module({
-    controllers: [PredictionController],
-    providers: [PredictionService],
-})
-
-export class PredictionModule {}
